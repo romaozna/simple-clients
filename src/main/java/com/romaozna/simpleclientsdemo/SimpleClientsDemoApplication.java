45ad8b93e63bc4cde91b67ac07374b693b363cf1
akhmerov.rr <akhmerov.rr@yandex.ru>
2023-09-01 13:24:32 +0500

    create db

---
+++ src/main/java/com/romaozna/simpleclientsdemo/SimpleClientsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.romaozna.simpleclientsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleClientsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleClientsDemoApplication.class, args);
+	}
+
+}
